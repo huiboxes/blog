15d4aead4f60de3bb6027aab2a3fa5814453d839
kylo <devhui@foxmail.com>
2022-09-03 15:48:50 +0800

    feat: 创建邮箱订阅项目

---
+++ email-subscription-panel/src/App.tsx
@@ -0,0 +1,42 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  ReadyPage,
+  ErrorComponent,
+  LoginPage,
+} from "@pankod/refine-antd";
+import "@pankod/refine-antd/dist/styles.min.css";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { DataProvider } from "@pankod/refine-strapi-v4";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+import { authProvider, axiosInstance } from "./authProvider";
+import { API_URL } from "./constants";
+
+function App() {
+  return (
+    <Refine
+      notificationProvider={notificationProvider}
+      ReadyPage={ReadyPage}
+      catchAll={<ErrorComponent />}
+      routerProvider={routerProvider}
+      authProvider={authProvider}
+      dataProvider={DataProvider(API_URL + `/api`, axiosInstance)}
+      LoginPage={LoginPage}
+      Title={Title}
+      Header={Header}
+      Sider={Sider}
+      Footer={Footer}
+      Layout={Layout}
+      OffLayoutArea={OffLayoutArea}
+    />
+  );
+}
+
+export default App;

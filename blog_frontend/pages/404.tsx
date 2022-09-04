fff98567e2dfebf63636a2f140908651c6af0912
kylo <huiboxes@gmail.com>
2022-09-04 15:43:32 +0800

    doc: 修改 404 提示

--- blog_frontend/pages/404.tsx
+++ blog_frontend/pages/404.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
         </h1>
         <p className="text-gray-600 dark:text-gray-400 mb-8">
           为什么要显示一个通用的404?似乎你找到了曾经存在的东西，或者你拼了什么错了。
-          我猜你拼写错了。你能再确认一下吗网址吗?
+          我猜你拼写错了。你能再确认一下网址吗?
         </p>
         <Link href="/">
           <a className="p-1 sm:p-4 w-64 font-bold mx-auto bg-gray-200 dark:bg-gray-800 text-center rounded-md text-black dark:text-white">
